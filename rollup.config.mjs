faa97d55858108996909780291e1312ddc1c9bbb
bhb <selre1@naver.com>
2025-11-23 14:42:46 +0900

    롤업 최적화 변환

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,21 +1,16 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
 
-export default {
+const umdConfig = {
   input: "src/index.js",
-  output: [
-    {
-      file: "dist/git-viewer.umd.js",
+  output: {
+      //file: "dist/git-viewer.umd.js",
+      file: "dist/git-viewer.js",
       format: "umd",
-      name: "GIT", // window.UndergroundViewer 로 노출될 이름
-      sourcemap: true,
-    },
-    {
-      file: "dist/git-viewer.esm.js",
-      format: "es",
-      sourcemap: true,
-    },
-  ],
+      name: "GIT", // 노출될 이름
+      sourcemap: false,
+  },
   // 현재 소스는 Cesium, jQuery를 import 하지 않고 window 글로벌로 쓰므로
   // external 설정이 필수는 아님. (작성된 코드만 번들됨)
   // 추후 'import Cesium from "cesium"' 같이 바꾸면 여기서 external/globals 세팅해줘야 함.
@@ -25,5 +20,37 @@ export default {
       browser: true,
     }),
     commonjs(),
+    terser({
+      format: {
+        comments: false,     // 주석 삭제
+      },
+      compress: {
+        drop_console: true,  // console.log 제거 (옵션)
+      },
+      mangle: true,          // 변수/함수명 난독
+    })
   ],
+  treeshake: "smallest"
 };
+
+const esmConfig = {
+  input: "src/index.js",
+  output: {
+    file: "dist/git-viewer.esm.js",
+    format: "es",
+    sourcemap: true,  // 개발용 디버그 편하게
+  },
+  external: [],
+  plugins: [
+    resolve({ browser: true }),
+    commonjs(),
+    terser({
+      format: { comments: false },
+      compress: { drop_console: false },
+      mangle: false,   // 이름은 그대로 두고, 공백/구조만 압축
+    }),
+  ],
+  treeshake: "smallest",
+};
+
+export default [umdConfig, esmConfig];

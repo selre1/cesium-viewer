16c7ef931a3337f355b47d25cfcc2af682b50f63
bhb <selre1@naver.com>
2025-12-22 15:38:47 +0900

    jsdoc 테스트

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import CesiumHandler from "./CesiumHandler.js";
 
-/**
- *  target: "#element" 또는 DOM Element
- *  options: { 3dtileset url, 관련 속성정보 url , info={ 타이틀, 이미지 src(svg,png), 높이(default 100)} }
- */
-export default async function CesiumViewer(target, options = {}) {
+export async function CesiumViewer(target, options = {}) {
   // target이 문자열이면 '#viewerRoot' 형태일 수도 있으니 '#' 제거해서 id로 사용
   const elementId = typeof target === "string" ? target.replace(/^#/, "") : (target && target.id);
   const viewer = await CesiumHandler.init(elementId, options);
@@ -42,3 +38,5 @@ export default async function CesiumViewer(target, options = {}) {
     addWebMapService
   };
 }
+
+export default CesiumViewer;
